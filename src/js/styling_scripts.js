980eda2a8c77dd11db3557b21fd6653abb7a6dae
anton <contact@sorridi.xyz>
2023-06-24 23:46:31 +0200

    Revert "Revert "Fixing visual: whole index.html""
    This reverts commit bd57e678c535c931e08faf988fb4a2b78a16b493.

--- src/js/styling_scripts.js
+++ src/js/styling_scripts.js
@@ -1,5 +1,10 @@
+
 export function modifyFontSize(size) {
     const element = document.getElementById("main-content");
     element.style.fontSize = size
+}
+
+export function modifyContentForOptionalList() {
+
 }
 
