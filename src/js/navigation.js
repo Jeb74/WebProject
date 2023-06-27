50d8f0c825c02a503a373e121f368a7ce5ec5e20
anton <contact@sorridi.xyz>
2023-06-27 17:18:50 +0200

    Fix JS

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,12 +2,13 @@ const navReference = document.getElementsByClassName("navigation")[0]
 const contentReference = document.getElementsByTagName("main")[0]
 const gNavBarPos = navReference.offsetTop
 
-export function trackCurrentPage() {
-    const cPage = window.location.href;
-    const elements = Array.from(document.getElementsByClassName("navigation-stage"))
-    const element = elements.find(e => cPage.search(e.getAttribute("href").split(".")[0]));
-    
-    element.style.backgroundColor = "#2A2A2A"
+export function trackCurrentPage() {     
+    let pages = Array.from(document.getElementsByClassName("navigation-stage")) 
+       
+    pages
+        .find(p => window.location.href.search(p.getAttribute("href").split(".")[0]) !== -1)
+        .style
+        .backgroundColor = "#2A2A2A"; 
 }
 
 export function onScrollSetNav() {
