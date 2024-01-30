a078ebb30d1c90a40074dab6c75bf3140cd9889a
anton <contact@sorridi.xyz>
2024-01-30 11:18:22 +0100

    A bit of cleanup, enhancements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,44 @@
 import {modifyFontSize} from "./styling_scripts.js";
 
-function onLoadGetPage() {
+function setupZoomButtons() 
+{
     const resizerSection = document.getElementById("resizer")
     const buttons = resizerSection.querySelectorAll("button")
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function() {
-            modifyFontSize(buttons[i].classList[0])
+    buttons.forEach(function(button)
+    {
+        button.onclick = function() 
+        {
+            modifyFontSize(button.classList[0])
         }
+    })
+}
+
+function setupLogoutButton() 
+{
+    const logoutButton = document.getElementById("logout")
+
+    if (logoutButton == null)
+        return;
+
+    logoutButton.onclick = function()
+    {
+        fetch("./logout", {
+            method: "POST",
+            credentials: "same-origin"
+        })
+        .then(result => {
+                if (result.ok) 
+                {
+                    console.log("logged out!")
+                    window.location.href = "./index.html";
+                }
+        })
     }
 }
 
-window.onload = function() { onLoadGetPage() }
+window.onload = function() 
+{ 
+    setupZoomButtons()
+    setupLogoutButton()
+}
